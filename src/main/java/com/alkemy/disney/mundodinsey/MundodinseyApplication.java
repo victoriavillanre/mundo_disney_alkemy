278fa2bf6189674bee24ee5d441b829331d293b5
Victoria <villanrevictoria@gmail.com>
2022-02-14 19:20:54 -0300

    Primer commit

---
+++ src/main/java/com/alkemy/disney/mundodinsey/MundodinseyApplication.java
@@ -0,0 +1,13 @@
+package com.alkemy.disney.mundodinsey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MundodinseyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MundodinseyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alkemy/disney/mundodinsey/MundodinseyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemy.disney.mundodinsey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MundodinseyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
